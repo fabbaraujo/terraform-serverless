3a0efcfff2b0f7e7f623e1f42f5362be1900246c
Fabrício Araújo <fabaraujo23@gmail.com>
2022-04-15 17:41:52 -0300

    add stream bookings

--- terraform/infra/bookings/dynamodb-bookings.tf
+++ terraform/infra/bookings/dynamodb-bookings.tf
@@ -9,6 +9,8 @@ resource "aws_dynamodb_table" "bookings" {
 
     write_capacity = "${var.write_capacity}"
     read_capacity = "${var.read_capacity}"
+    stream_enabled = true
+    stream_view_type = "NEW_IMAGE"
 }
 
 resource "aws_ssm_parameter" "dynamodb_bookings_table" {
